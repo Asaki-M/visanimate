836991629bd9d5c6d4cd468705d8bf30f6867588
Qinghuan <1041220793@qq.com>
2022-05-24 22:06:44 +0800

    feat: can change preview style by editor

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -2,17 +2,28 @@ import { Input, Button } from 'antd'
 import { CheckOutlined, CopyOutlined } from '@ant-design/icons'
 import styles from './index.module.scss'
 import { propsInterface } from './interface'
+import { useCallback, useState } from 'react'
+import { dispatch } from 'use-bus'
 
 const { TextArea } = Input
 
 function Editor(props: propsInterface) {
   const { canEditor } = props
+  const [code, setCode] = useState('')
+  const addPreviewStyle = useCallback(() => {
+    // need to formatter code
+    dispatch({ type: 'ADD_PREVIEW_STYLE', payload: code })
+  }, [code])
   return (
     <div className={styles['editor-contain']}>
       {canEditor ? (
         <div className={styles['can-editor']}>
           <div className={styles.title}>Preview box style</div>
-          <Button type="link" icon={<CheckOutlined />}></Button>
+          <Button
+            type="link"
+            icon={<CheckOutlined />}
+            onClick={addPreviewStyle}
+          ></Button>
         </div>
       ) : (
         <div className={styles['cannot-editor']}>
@@ -24,9 +35,10 @@ function Editor(props: propsInterface) {
       <TextArea
         placeholder={canEditor ? 'Can editor preview box style here' : ''}
         disabled={!canEditor}
-        allowClear={!canEditor}
+        allowClear={!!canEditor}
         rows={12}
-        maxLength={12}
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
       ></TextArea>
     </div>
   )
--- src/components/preview/index.tsx
+++ src/components/preview/index.tsx
@@ -1,9 +1,43 @@
 import styles from './index.module.scss'
+import useBus from 'use-bus'
+
+interface StyleObj {
+  prop: string
+  value: string
+}
+
+function getStyleList(style: string): StyleObj[] {
+  const styles = style.split('\n')
+  const list = []
+  for (let i = 0; i < styles.length; i++) {
+    const syl = styles[i].split(':')
+    const key = syl[0].trim()
+    const value = syl[1].trim().replaceAll(';', '')
+    list.push({ prop: key, value: value })
+  }
+  return list
+}
 
 function Preview() {
+  useBus(
+    'ADD_PREVIEW_STYLE',
+    ({ type, payload }) => {
+      const box = document.querySelector('#preview-box') as HTMLElement
+      if (payload === '') {
+        box.setAttribute('style', '')
+        return
+      }
+      const styleList: StyleObj[] = getStyleList(payload)
+      styleList.forEach((item) => {
+        const { prop, value } = item
+        box.style[prop] = value
+      })
+    },
+    []
+  )
   return (
     <article className={styles['preview-layout']}>
-      <div className={styles['preview-box']}></div>
+      <div id="preview-box" className={styles['preview-box']}></div>
     </article>
   )
 }
