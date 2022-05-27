1b6161e65e7307d9bde194aacff6438a46c5f273
Qinghuan <1041220793@qq.com>
2022-05-27 12:54:06 +0800

    style: add play button

--- src/components/animateForm/index.tsx
+++ src/components/animateForm/index.tsx
@@ -3,6 +3,8 @@ import TableGroup from './tableGroup'
 import AddAnimate from './addAnimate'
 import { AnimateFormData } from './interface'
 import { useState } from 'react'
+import { Button } from 'antd'
+import { PlayCircleOutlined } from '@ant-design/icons'
 
 function AnimateForm() {
   const [animateList, setAnimateList] = useState<AnimateFormData[]>([])
@@ -15,7 +17,14 @@ function AnimateForm() {
     <div className={styles['contain']}>
       <header className={styles.header}>
         <div className={styles.title}>Animate List</div>
-        <AddAnimate sendData={getFormData}></AddAnimate>
+        <div className={styles.controls}>
+          <AddAnimate sendData={getFormData}></AddAnimate>
+          <Button
+            type="primary"
+            className={styles.play}
+            icon={<PlayCircleOutlined />}
+          ></Button>
+        </div>
       </header>
       <main className={styles.main}>
         <TableGroup animateList={animateList}></TableGroup>
